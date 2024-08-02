f4bd021f46e32bfa800d8cf5e2299308ffdb7a2e
malikkusman <42khan0@gmail.com>
2024-08-02 10:58:31 +0500

    Update Backend

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const connectDB = require("./utils/db");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const AIRoutes = require("./routes/airoutes");
 
 //middleware
 app.use(cors());
@@ -17,6 +18,7 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use("/api", userRoutes);
 app.use("/api", taskRoutes);
+app.use("/api", AIRoutes);
 
 PORT = process.env.PORT || 5000;
 
